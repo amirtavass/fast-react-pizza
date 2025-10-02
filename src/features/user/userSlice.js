463b6e48bf377ab76e30187b2fa4946028457337
amirtavass <amirtavass62@gmail.com>
2025-10-02 14:53:54 +0100

    added redux to the project and connected react to redux

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { username: 'test' };
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.name = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+export default userSlice.reducer;
