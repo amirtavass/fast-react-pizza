9b0491b1f6fcdf3723447ea166b9ae4a2b50888a
amirtavass <amirtavass62@gmail.com>
2025-10-05 15:49:39 +0100

    Modeling the user,the cart stage 1 using REDUX TOOLKIT

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,13 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { username: 'test' };
+const initialState = { username: '' };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     updateName(state, action) {
-      state.name = action.payload;
+      state.username = action.payload;
     },
   },
 });
