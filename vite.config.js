7b5fd8e3c388d44387f923757febf1d96233a551
amirtavass <amirtavass62@gmail.com>
2026-07-09 16:52:04 +0100

    packages updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 import eslint from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     eslint({
       // This will run ESLint when Vite starts
